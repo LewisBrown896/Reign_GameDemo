dfb22c30ca132b7397e549fc8f886907e9136df7
Lewis_Brown-cst216 <zombielewy@outlook.com>
2020-04-14 06:11:26 -0700

    older versions of of the game gradually updated

---
+++ Older Version .01-0.9 (12-15-2020)-(3-2-2020)/Code/PlayerArmor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArmor : MonoBehaviour
+{
+    //maxArmor is set to 100
+    public int maxArmor = 100;
+    public int currentArmor;
+
+    public ArmorBar armorBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentArmor = maxArmor;
+        armorBar.SetMaxArmor(maxArmor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) // on left mouse click take armor damage -20
+        {
+            TakeDamage(20);
+        }
+    }
+
+    //each time left click is pressed the amount of armor goes down
+    //basic armor calculator
+    //will be replaced when enemies are added
+    void TakeDamage(int damage)
+    {
+        currentArmor -= damage;
+
+        armorBar.SetArmor(currentArmor);
+    }
+}
